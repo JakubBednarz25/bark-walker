7921a710ff5964810d4ea7a2b51d55017125052c
Jakub Bednarz <73561350+JakubBednarz25@users.noreply.github.com>
2023-10-07 18:08:39 +0100

    Add application name, navigation links, and login button (nonfunctional yet)  to header
    - Added the application name to the header for branding purposes.
    - Included navigation links
    - Added a login button to the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,21 @@
+import styles from "./Header.module.scss";
+
+import Link from "next/link";
+
 const Header = () => {
-    return (<header></header>)
-}
+  return (
+    <header className={styles.header}>
+      <Link className={styles.businessName} href={"/"}>BarkWalker</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/"}>Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+      <Link href={"/"}>Log in</Link>
+    </header>
+  );
+};
 
 export default Header;
